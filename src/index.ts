7616ffe282d3f56e825cc01dec3eeb54957f5de9
Max Pijittum <mahasak@gmail.com>
2025-02-15 11:26:34 +0800

    Add starter view

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import { Elysia } from "elysia";
 import { apiRoutes } from "./routes";
+import { html, Html } from '@elysiajs/html'
+import * as elements from "typed-html"
 
+import main from './views/main'
 const app = new Elysia()
+  .use(html())
+  
+  .get("/", () => "🦊 Sigma core v.0.0.1",  {
+		afterResponse() {
+			console.log('After response')
+		}
+	})
+  .get('/html', ({ set, error }) => {
+    set.headers["content-type"] = 'text/html; charset=utf8'
+    return main
+  }
+  , {
+		afterResponse() {
+			console.log('After response')
+		}
+	})
   .use(apiRoutes)
-  .get("/", () => "Hello Elysia")
-  .get("/signature", () => "🦊 Sigma core v.0.0.1")
   .listen(3000);
 
 console.log(
